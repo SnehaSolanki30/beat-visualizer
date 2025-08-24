c0b10454f111b07cadfb7f1d24df0e8306d971e6
SnehaSolanki30 <snehasolanki3095@gmail.com>
2025-08-24 15:03:12 +0530

    Remove all files

--- GSSOC/job.js
+++
@@ -1,8 +0,0 @@
-// Auto-update year
-document.addEventListener("DOMContentLoaded", function () {
-  const yearSpan = document.querySelector(".footer-bottom p");
-  if (yearSpan) {
-    const currentYear = new Date().getFullYear();
-    yearSpan.innerHTML = `© ${currentYear} JobSync. All rights reserved.`;
-  }
-});
